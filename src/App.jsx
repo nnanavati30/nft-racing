e1dc42e0806153ed53a73ef9fbc8327d277ff650
Naman Nanavati <nnanavati@fireblocks.com>
2024-06-03 18:13:07 +1000

    handle login

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,20 @@ import HomePage from './pages/Homepage';
 function App() {
   const { signer, address } = useWeb3();
 
+  const handleLogin = async () => {
+    if (window.ethereum) {
+      await window.ethereum.enable();
+    }
+  };
+
   return (
     <div className='flex items-center justify-center min-h-screen flex-col relative'>
       <HomePage />
-      <div className='absolute bg-white p-2 rounded-md'>{address}</div>
+      {address ? (
+        <div className='absolute bg-white py-2 px-4 rounded-md'>{address}</div>
+      ) : (
+        <button className='absolute bg-white py-2 px-4 rounded-md' onClick={handleLogin}>Please login to your MetaMask wallet!</button>
+      )}
     </div>
   )
 }
